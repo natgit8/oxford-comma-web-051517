20f14f61928933e4fb11f05027e22bfe4ed6ff16
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-04-28 17:17:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.length == 1
+  array.join('')
 
-end
+elsif array.length == 2
+  array.join(' and ')
+
+elsif array.length >= 3
+  array[-1] = "and #{array[-1]}"
+  array.join(', ')
+
+  else
+    nil
+  end
+end  
